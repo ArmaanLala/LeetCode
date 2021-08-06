6c404e009fc8415935b426dce8169402d5b8afc3
Armaan Lala <armaanlala@gmail.com>
2021-08-06 09:45:37 -0400

    Time: 40 ms (81.18%), Space: 14.5 MB (44.08%) - LeetHub

---
+++ isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dict = {}
+        for i in range(len(s)):
+            if s[i] in dict :
+                if not(dict[s[i]] == t[i]):
+                    # print(dict[s[i] + "  " + t[i]])
+                    return False
+            else:
+                if (t[i] in dict.values()):
+                    return False
+                else:
+                    dict[s[i]] = t[i]
+        return True
